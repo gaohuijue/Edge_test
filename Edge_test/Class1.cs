d9669278dcc8f920e0b02500a77d29b754b5399d
gaohuijue <gaohuijue@shinow.com>
2018-02-05 14:40:33 +0800

    添加项目文件。

---
+++ Edge_test/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edge_test
+{
+    public class Class1
+    {
+    }
+}
